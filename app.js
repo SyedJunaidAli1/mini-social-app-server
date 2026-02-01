a4929454b41d469d8129a40d95b12bc0c79c972d
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-01 15:55:21 +0530

    config/db.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const FRONTEND_ENDPOINT = process.env.FRONTEND_ENDPOINT;
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: FRONTEND_ENDPOINT,
+    credentials: true,
+  }),
+);
+
+export default app;
