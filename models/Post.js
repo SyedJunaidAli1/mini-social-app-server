2bc272f82fb2f9310a026efd1e64df2f438bf5d0
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-01 16:38:25 +0530

    model created user,post

---
+++ models/Post.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: String,
+
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      default: "",
+    },
+
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  { timestamps: true },
+);
+
+postSchema.index({ createdAt: -1 });
+
+postSchema.pre("save", function (next) {
+  if (!this.text && !this.imageUrl) {
+    return next(new Error("Post must contain text or an image"));
+  }
+  next();
+});
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
---
+++ models/User.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
