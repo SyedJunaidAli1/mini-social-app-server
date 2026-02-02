e707774d93496892bf3e00b9dce646e6ec2bb29a
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-02 19:00:57 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const FRONTEND_ENDPOINT = process.env.FRONTEND_ENDPOINT;
 
 app.use(express.json());
 
+console.log("FRONTEND:", process.env.FRONTEND_ENDPOINT);
+
 app.use(
   cors({
-    origin: FRONTEND_ENDPOINT,
+    
+    origin: process.env.FRONTEND_ENDPOINT,
     credentials: true,
   }),
 );
